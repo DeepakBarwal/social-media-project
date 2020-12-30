69610ea4e00adb44d3f493b29aa788e05b2e0fc8
DeepakBarwal <deepakbarwal98@gmail.com>
2020-12-30 21:08:30 +0530

    get all posts and show on home

--- src/public/app/social-common.js
+++ src/public/app/social-common.js
@@ -1,6 +1,7 @@
 $(() => {
   $('#navbar').load('../components/navbar.html', loginIfNeeded);
   $('#footer').load('../components/footer.html');
+  $('#content').load('../components/all-posts.html', loadPosts);
 });
 
 function loginIfNeeded() {
@@ -20,4 +21,28 @@ function loginIfNeeded() {
     console.log('resuming session as ' + currentUser.username);
     $('#nav-username').text(currentUser.username);
   }
+}
+
+function loadPosts() {
+  $.get('/api/posts', (posts) => {
+    for (let p of posts) {
+      $('#posts-container').append(
+        $(`
+        <div class="col-4">
+          <div class="card m-2">
+          <div class="card-body">
+            <h5 class="card-title">${p.title}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">${p.user.username}</h6>
+            <p class="card-text">${p.body.substr(
+              0,
+              200
+            )} <a href='#'>...read more</a></p>
+            <a href="#" class="card-link">Comment</a>
+            <a href="#" class="card-link">Like</a>
+          </div>
+          </div>
+        </div>`)
+      );
+    }
+  });
 }
